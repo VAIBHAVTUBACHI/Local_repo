72cfe5cec12ca2d644d8ef242682f1e0775664df
VAIBHAVTUBACHI <vtubachi@gmail.com>
2024-09-08 21:48:59 +0530

    small change in feature678

--- test.c
+++ test.c
@@ -2,5 +2,5 @@
 
 int main(){
     printf("Hi i am a new repo");
-    printf("I am feature 6 "); printf("I am feature 6 "); printf("I am feature 6 ");
+    printf("I am feature 6 "); printf("I am feature dfdghd "); printf("I am feature 6dsfsf ");
 }
